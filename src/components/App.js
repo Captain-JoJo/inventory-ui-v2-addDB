2a98b83c34aba65a901141673e6b7038f98e1c38
Tessa Seiders <tessa.seiders@gmail.com>
2021-02-06 00:04:12 -0600

    added basic code

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import NavBar from './navbar/NavBar'
+import Header from './header/Header'
+import Footer from './footer/Footer'
+import Inventory from './inventory/Inventory'
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Header />
+      <Inventory />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
