970874958725b70a326a84227408ff78b022060e
Tessa Seiders <tessa.seiders@gmail.com>
2021-02-13 21:15:01 -0600

    updated to only delete 1 item

--- src/components/inventory/Details.js
+++ src/components/inventory/Details.js
@@ -4,16 +4,16 @@ import axios from 'axios'
 export default function GetInventoryData() {
 const [items, setItems] = useState([])
 
-// const getItems = async () => {
-//     axios.get("https://inventoryv2api.herokuapp.com/getData").then(res => {
-//         setItems(res.data)
-//     })
-// }
 const getItems = async () => {
-    axios.get("http://localhost:5000/getData").then(res => {
+    axios.get("https://inventoryv2api.herokuapp.com/getData").then(res => {
         setItems(res.data)
     })
 }
+// const getItems = async () => {
+//     axios.get("http://localhost:5000/getData").then(res => {
+//         setItems(res.data)
+//     })
+// }
 
 function deleteItem(id) {
     setItems(prevItems => {
@@ -24,20 +24,8 @@ function deleteItem(id) {
     })
 }
 
-// const deleteItems = async () => {
-//     axios.get("https://inventoryv2api.herokuapp.com/deleteAll").then(res => {
-//         if (setItems > 0) {
-//             setItems(res.data)
-//         } else {
-//             deleteItem()            
-//             console.log('deleteItems function called', deleteItem());
-//         }
-//         console.log(res.data);
-//     })
-// }
-
 const deleteItems = async () => {
-    axios.get("http://localhost:5000/deleteAll").then(res => {
+    axios.get("https://inventoryv2api.herokuapp.com/deleteAll").then(res => {
         if (setItems > 0) {
             setItems(res.data)
         } else {
@@ -48,6 +36,30 @@ const deleteItems = async () => {
     })
 }
 
+const deleteItems = async () => {
+    axios.get("https://inventoryv2api.herokuapp.com/deleteOneItem").then(res => {
+        if (setItems > 0) {
+            setItems(res.data)
+        } else {
+            deleteItem()            
+            console.log('deleteItems function called', deleteItem());
+        }
+        console.log(res.data);
+    })
+}
+
+// const deleteItems = async () => {
+//     axios.get("http://localhost:5000/deleteAll").then(res => {
+//         if (setItems > 0) {
+//             setItems(res.data)
+//         } else {
+//             deleteItem()            
+//             console.log('deleteItems function called', deleteItem());
+//         }
+//         console.log(res.data);
+//     })
+// }
+
 
     return (
         <div className="grid-container">
@@ -55,20 +67,17 @@ const deleteItems = async () => {
                 <button className="fetch-button" onClick={getItems}>
                     Get Items
                 </button>
-                <button className="fetch-button" onClick={getItems}>
-                    Delete Items
+                <button className="fetch-button" onClick={deleteItems}>
+                    Delete All Items
                 </button>
             </div>
             <div>
-                
-                    {items.map(item =>(
-                        <div>
-                            <span name={item.name} onClick={deleteItems}>X</span>
-                            <span>{item.name}</span>
-                        </div>
-                       
-                    ))}
-                
+                {items.map(item =>(
+                    <div>
+                        <span name={item.name} onClick={deleteOneItem}>Remove</span>
+                        <span>{item.name}</span>
+                    </div>
+                ))}
             </div>
         </div>
     )
