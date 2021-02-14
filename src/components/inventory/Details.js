9e69c8c17a3495637eb3c96e5009ad7e1b5807a7
Tessa Seiders <tessa.seiders@gmail.com>
2021-02-13 21:58:47 -0600

    added new create button

--- src/components/inventory/Details.js
+++ src/components/inventory/Details.js
@@ -2,63 +2,85 @@ import React, { useState } from 'react'
 import axios from 'axios'
 
 export default function GetInventoryData() {
-const [items, setItems] = useState([])
 
-const getItems = async () => {
-    axios.get("https://inventoryv2api.herokuapp.com/getData").then(res => {
-        setItems(res.data)
-    })
-}
-// const getItems = async () => {
-//     axios.get("http://localhost:5000/getData").then(res => {
-//         setItems(res.data)
-//     })
-// }
+    const [inputText, setInputText] = useState("")
+    const [items, setItems] = useState([])
 
-function deleteItem(id) {
-    setItems(prevItems => {
-        return prevItems.filter((item, index) => {
-            console.log('deleting items', item);
-            return item.id !== id
-        })
-    })
-}
+    function handleChange(event) {
+        const newValue = event.target.value
+        setInputText(newValue)
+    }
 
-const deleteAll = async () => {
-    axios.get("https://inventoryv2api.herokuapp.com/deleteAll").then(res => {
-        if (setItems > 0) {
+    const insertItems = async () => {
+        axios.get("https://inventoryv2api.herokuapp.com/insertData").then(res => {
             setItems(res.data)
-        } else {
-            deleteItem()            
-            console.log('deleteItems function called', deleteItem());
-        }
-        console.log(res.data);
-    })
-}
+        })
+    }
 
-const deleteOne = async () => {
-    axios.get("https://inventoryv2api.herokuapp.com/deleteOne").then(res => {
-        deleteItem()
-        console.log('Only deleting one');
-    })
-}
+    const getItems = async () => {
+        axios.get("https://inventoryv2api.herokuapp.com/getData").then(res => {
+            setItems(res.data)
+        })
+    }
+    // const getItems = async () => {
+    //     axios.get("http://localhost:5000/getData").then(res => {
+    //         setItems(res.data)
+    //     })
+    // }
 
-// const deleteItems = async () => {
-//     axios.get("http://localhost:5000/deleteAll").then(res => {
-//         if (setItems > 0) {
-//             setItems(res.data)
-//         } else {
-//             deleteItem()            
-//             console.log('deleteItems function called', deleteItem());
-//         }
-//         console.log(res.data);
-//     })
-// }
+    function deleteItem(id) {
+        setItems(prevItems => {
+            return prevItems.filter((item, index) => {
+                console.log('deleting items', item);
+                return item.id !== id
+            })
+        })
+    }
+
+    const deleteAll = async () => {
+        axios.get("https://inventoryv2api.herokuapp.com/deleteAll").then(res => {
+            if (setItems > 0) {
+                setItems(res.data)
+            } else {
+                deleteItem()            
+                console.log('deleteItems function called', deleteItem());
+            }
+            console.log(res.data);
+        })
+    }
+
+    const deleteOne = async () => {
+        axios.get("https://inventoryv2api.herokuapp.com/deleteOne").then(res => {
+            deleteItem()
+            console.log('Only deleting one');
+        })
+    }
+
+    // const deleteItems = async () => {
+    //     axios.get("http://localhost:5000/deleteAll").then(res => {
+    //         if (setItems > 0) {
+    //             setItems(res.data)
+    //         } else {
+    //             deleteItem()            
+    //             console.log('deleteItems function called', deleteItem());
+    //         }
+    //         console.log(res.data);
+    //     })
+    // }
 
 
     return (
         <div className="grid-container">
             <div>
+                <input 
+                    onChange={handleChange}
+                    type="text"
+                    placeholder="Add Item"
+                    value={inputText}
+                />
+                <button onClick={insertItems}>
+                    <span>Add Item</span>
+                </button>
                 <button className="fetch-button" onClick={getItems}>
                     Get Items
                 </button>
