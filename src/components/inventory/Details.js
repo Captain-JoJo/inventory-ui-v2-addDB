0c1b30037ffc774c52162c2d3657b2ea7ccc86e7
Tessa Seiders <tessa.seiders@gmail.com>
2021-02-13 18:12:18 -0600

    updated route

--- src/components/inventory/Details.js
+++ src/components/inventory/Details.js
@@ -4,11 +4,50 @@ import axios from 'axios'
 export default function GetInventoryData() {
 const [items, setItems] = useState([])
 
+// const getItems = async () => {
+//     axios.get("https://inventoryv2api.herokuapp.com/getData").then(res => {
+//         setItems(res.data)
+//     })
+// }
 const getItems = async () => {
-    axios.get("https://inventoryv2api.herokuapp.com/getData").then(res => {
+    axios.get("http://localhost:5000/getData").then(res => {
         setItems(res.data)
     })
 }
+
+function deleteItem(id) {
+    setItems(prevItems => {
+        return prevItems.filter((item, index) => {
+            console.log('deleting items', item);
+            return item.id !== id
+        })
+    })
+}
+
+// const deleteItems = async () => {
+//     axios.get("https://inventoryv2api.herokuapp.com/deleteAll").then(res => {
+//         if (setItems > 0) {
+//             setItems(res.data)
+//         } else {
+//             deleteItem()            
+//             console.log('deleteItems function called', deleteItem());
+//         }
+//         console.log(res.data);
+//     })
+// }
+
+const deleteItems = async () => {
+    axios.get("http://localhost:5000/deleteAll").then(res => {
+        if (setItems > 0) {
+            setItems(res.data)
+        } else {
+            deleteItem()            
+            console.log('deleteItems function called', deleteItem());
+        }
+        console.log(res.data);
+    })
+}
+
 
     return (
         <div className="grid-container">
@@ -16,15 +55,20 @@ const getItems = async () => {
                 <button className="fetch-button" onClick={getItems}>
                     Get Items
                 </button>
+                <button className="fetch-button" onClick={getItems}>
+                    Delete Items
+                </button>
             </div>
             <div>
-                <ul>
+                
                     {items.map(item =>(
-                        <li>
-                            {`My fruits: ${item.name} ${item.rating} ${item.review}`}
-                        </li>
+                        <div>
+                            <span name={item.name} onClick={deleteItems}>X</span>
+                            <span>{item.name}</span>
+                        </div>
+                       
                     ))}
-                </ul>
+                
             </div>
         </div>
     )
