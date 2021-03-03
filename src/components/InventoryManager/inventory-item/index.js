fe341cc375de2c38a670d6aa91c1ea5ff29dcf43
Tessa Seiders <tessa.seiders@gmail.com>
2021-03-02 18:18:25 -0600

    adding new file for extracting functions outside of home details

---
+++ src/components/InventoryManager/inventory-item/index.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import {
+    insertItem,
+    getAllItems,
+    deleteAllItems,
+    deleteOneItem,
+  } from "../../../api/inventoryItem";
+    
+  export async function addNewItem(item, setItems, items) {
+    try {
+      const _id = await insertItem(item);
+
+      const updatedItems = [...items, { _id: _id, name: item}];
+      setItems(updatedItems);
+    } catch (error) {
+      console.log("sql error", error);
+    }
+  }
+
+  export async function displayItems(setItems) {
+    const results = await getAllItems();
+    setItems(results);
+  }
+
+  export async function deleteAll(setItems) {
+    try {
+      await deleteAllItems();
+      setItems([]);
+    } catch (error) {
+      console.log("sql error", error);
+    }
+  }
+
+  export async function deleteOne(id, setItems, items) {
+    try {
+      const results = await deleteOneItem(id);
+      setItems(items.filter((item) => item._id !== id));
+    } catch (error) {
+      console.log("sql error", error);
+    }
+  }
+//}
--- src/components/InventoryManager/inventory-list/index.js
+++ src/components/InventoryManager/inventory-list/index.js
@@ -1,78 +1,27 @@
-import React from 'react'
+import React from "react";
 import "../HomeDetails.css";
 
 export default function List(props) {
-
- const displayItems = (props) => {
-    console.log('trying to see if there are any props here', props);
-
-    if(props.name.length > 0){
-        return (
-             <div className="ListContainer">
-                <ul>
-                    <li>
-                        <span>{props.name}</span>
-                        <span className="button-group">
-                            <span
-                                className="button"
-                                _id={props._id}
-                                name={props.name}
-                                onClick={() => props.onChecked(props._id)}
-                            >
-                                Remove
-                            </span>
-                        </span>
-                    </li>
-                </ul>
-            </div>
-        )
-    } else {
-        return (<h3>I have no items to display yet</h3>)
-    }
- }
- return (
-     <> {(displayItems(props))} </>
- )
+  const displayItem = (props) => {
+    return (
+      <div className="ListContainer">
+        <ul>
+          <li>
+            <span>{props.name}</span>
+            <span className="button-group">
+              <span
+                className="button"
+                _id={props._id}
+                name={props.name}
+                onClick={() => props.onChecked(props._id)}
+              >
+                Remove
+              </span>
+            </span>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+  return <> {displayItem(props)} </>;
 }
-
-
-
-
-
-/*
-const [items, setItems] = useState([]);
-
-    async function deleteOne(id) {
-        console.log("initial id", id);
-        try {
-        const results = await deleteOneItem(id);
-        setItems(items.filter((item) => item._id !== id));
-        console.log("const results from the await deleteOneItem", results);
-        } catch (error) {
-        console.log("sql error", error);
-        }
-    }
-
-    return(
-        <div className="ListContainer">
-            <ul>
-            {items.map((item) => (
-                <div key={item._id}>
-                <li>
-                    <span>{item.name}</span>
-                    <span className="button-group">
-                    <span
-                        className="button"
-                        name={item.name}
-                        onClick={() => deleteOne(item._id)}
-                    >
-                        Remove
-                    </span>
-                    </span>
-                </li>
-                </div>
-            ))}
-            </ul>
-        </div>
-    )
-    */
